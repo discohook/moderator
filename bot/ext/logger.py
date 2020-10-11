f5d1b8a2d58423293233736ccfa059b9ebd22d9b
vivi <me@vivi.fyi>
2020-10-11 14:23:07 +0200

    Create history command

--- bot/ext/logger.py
+++ bot/ext/logger.py
@@ -39,6 +39,46 @@ class Logger(commands.Cog):
 
         await log_channel.send(embed=embed)
 
+    @commands.group(aliases=["hist"])
+    @commands.has_guild_permissions(manage_messages=True)
+    async def history(self, ctx: commands.Context):
+        """Group of commands to browse message history"""
+        await ctx.send_help("history")
+
+    @history.command(name="get")
+    @commands.has_guild_permissions(manage_messages=True)
+    async def history_get(self, ctx: commands.Context, message_id: int, version=0):
+        """Gets a message by its ID and it's version
+
+        Versions start at zero, each indexed edit adds one
+        """
+
+        content_data = await self.bot.db.fetchrow(
+            """
+            SELECT message_history.content, message_metadata.* FROM message_history
+            JOIN message_metadata ON (message_metadata.message_id = message_history.message_id)
+            WHERE message_history.message_id = $1
+            ORDER BY version_at
+            LIMIT 1 OFFSET $2
+            """,
+            message_id,
+            version,
+        )
+
+        channel = self.bot.get_channel(content_data["channel_id"])
+        author = channel.guild.get_member(content_data["author_id"])
+
+        embed = discord.Embed(
+            title=f"Message {message_id}",
+            url=f"https://discord.com/channels/{channel.guild.id}/{channel.id}/{message_id}",
+            description=content_data["content"],
+        )
+        embed.set_author(
+            name=f"{author} \N{BULLET} ({author.id})",
+            url=f"https://discord.com/users/{author.id}",
+            icon_url=author.avatar_url,
+        )
+
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         if message.author.bot or not message.guild:
@@ -124,7 +164,7 @@ class Logger(commands.Cog):
             f"\n{diff_message(old_content, event.data['content'], max_len=250)}"
         )
         embed.set_author(
-            name=f'{author} \N{BULLET} ({author.id})',
+            name=f"{author} \N{BULLET} ({author.id})",
             url=f"https://discord.com/users/{author.id}",
             icon_url=author.avatar_url,
         )
@@ -156,7 +196,7 @@ class Logger(commands.Cog):
             f"\n{cut_words(escape(stored_data['content']), max_len=250, end=' **... [cut off]**')}"
         )
         embed.set_author(
-            name=f'{author} \N{BULLET} ({author.id})',
+            name=f"{author} \N{BULLET} ({author.id})",
             url=f"https://discord.com/users/{author.id}",
             icon_url=author.avatar_url,
         )
